7f0d779917e2edebeda752b4bf76a4c2eefe1bdc
Guzmance <00031119@uca.edu.sv>
2023-05-30 23:12:32 -0600

    Add: Card and Buttons Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Components
+import { Button } from './Components/Button';
+import { Card } from './Components/Card';
+// Styles
+import './sass/App.scss';
+// Icons
+import { TiArrowLeftOutline } from 'react-icons/ti';
+import { TiArrowRightOutline } from 'react-icons/ti';
+// Hooks
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [pokemonId, setPokemonId] = useState(1);
+
+  function prevClick() {
+    pokemonId === 1 ? setPokemonId(1) : setPokemonId(pokemonId - 1);
+  }
+
+  function nextClick() {
+    setPokemonId(pokemonId + 1);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="card_container">
+        <Card />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="buttons-container">
+        <Button icon={<TiArrowLeftOutline />} handleClick={prevClick} />
+        <Button icon={<TiArrowRightOutline />} handleClick={nextClick} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export { App };
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import '../sass/Card.scss';
+
+const Card = () => {
+  return (
+    <div className="card">
+      <p className="card_name">Carlos</p>
+      <div className="card_circle"></div>
+      <img className="card_img" src="" alt="pokemon img" />
+    </div>
+  );
+};
+
+export { Card };
