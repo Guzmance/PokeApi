5aae846022e3538f30bbceb6a160ad21efc4a6b0
Guzmance <00031119@uca.edu.sv>
2023-05-31 00:07:08 -0600

    Add: API Consuption

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,46 @@ import './sass/App.scss';
 import { TiArrowLeftOutline } from 'react-icons/ti';
 import { TiArrowRightOutline } from 'react-icons/ti';
 // Hooks
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [pokemonId, setPokemonId] = useState(1);
+  const [pokemonEvolutions, setPokemonEvolutions] = useState([]);
+
+  useEffect(() => {
+    getEvolutions(pokemonId);
+  }, [pokemonId]);
+
+  async function getEvolutions(id) {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/evolution-chain/${id}/`
+    );
+    const data = await response.json();
+
+    let pokemonEvolutionArray = [];
+    let pokemonLv1 = data.chain.species.name;
+    let pokemonLv1Img = await getPokemonImg(pokemonLv1);
+    pokemonEvolutionArray.push([pokemonLv1, pokemonLv1Img]);
+
+    if (data.chain.evolves_to.lenght != 0) {
+      let pokemonLv2 = data.chain.evolves_to[0].species.name;
+      let pokemonLv2Img = await getPokemonImg(pokemonLv2);
+      pokemonEvolutionArray.push([pokemonLv2, pokemonLv2Img]);
+
+      if (data.chain.evolves_to[0].evolves_to.lenght != 0) {
+        let pokemonLv3 = data.chain.evolves_to[0].evolves_to[0].species.name;
+        let pokemonLv3Img = await getPokemonImg(pokemonLv3);
+        pokemonEvolutionArray.push([pokemonLv3, pokemonLv3Img]);
+        setPokemonEvolutions(pokemonEvolutionArray);
+      }
+    }
+  }
+
+  async function getPokemonImg(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+    const data = await response.json();
+    return data.sprites.other['official-artwork'].front_default;
+  }
 
   function prevClick() {
     pokemonId === 1 ? setPokemonId(1) : setPokemonId(pokemonId - 1);
@@ -23,7 +59,9 @@ const App = () => {
   return (
     <>
       <div className="card_container">
-        <Card />
+        {pokemonEvolutions.map((pokemon) => (
+          <Card key={pokemon[0]} name={pokemon[0]} img={pokemon[1]} />
+        ))}
       </div>
       <div className="buttons-container">
         <Button icon={<TiArrowLeftOutline />} handleClick={prevClick} />
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,11 @@
 import '../sass/Card.scss';
 
-const Card = () => {
+const Card = ({ name, img }) => {
   return (
     <div className="card">
-      <p className="card_name">Carlos</p>
+      <p className="card_name">{name}</p>
       <div className="card_circle"></div>
-      <img className="card_img" src="" alt="pokemon img" />
+      <img className="card_img" src={img} alt="pokemon img" />
     </div>
   );
 };
